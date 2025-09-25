e9b0e20ceda73c89122716decea1f3c7473d9d22
Farouk Umar Sedick <sadikalhanssah@gmail.com>
2025-09-25 09:37:44 +0000

    feat: update map repository

--- lib/core/constants/server_constants.dart
+++ lib/core/constants/server_constants.dart
@@ -1,3 +1,4 @@
 class ServerConstants {
   static const String baseUrl = 'https://trotro-hailing-authentication-servi.vercel.app';
+  static const String microserviceUrl = 'https://smart-trotro-map-microservice.vercel.app/';
 }
